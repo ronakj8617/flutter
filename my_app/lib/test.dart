3b812b2db1a2f9701c549c54e47bbfae794921e7
Ronak Parmar <97673784+ronakj8617@users.noreply.github.com>
2022-03-18 14:06:30 +0530

    Firebase added

--- my_app/lib/test.dart
+++ my_app/lib/test.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/Updated%20Files/my_styles.dart';
 import 'package:firebase_core/firebase_core.dart';
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-
 
   // await Firebase
   runApp(MaterialApp(
@@ -15,12 +13,30 @@ void main() {
 }
 
 class Home extends StatelessWidget {
-   Home({ Key? key }) : super(key: key);
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  Home({Key? key}) : super(key: key);
   // final Future<FirebaseApp> _initialization=FirebaseApp.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text('Error'));
+          }
+
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Padding(
+              padding: const EdgeInsets.all(160.0),
+              child: Center(
+                child: Column(
+                    children: [CircularProgressIndicator(), Text('Done')]),
+              ),
+            );
+          }
+
+          return CircularProgressIndicator();
+        });
   }
 }
